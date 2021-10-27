1abb3580941e163dd4dc3dca9eb14a945800d5d7
Niels Lanting <niels@nielslanting.com>
2021-10-27 19:03:00 +0200

    fix: dont crash when allow_alogrithm is set with invalid key

--- Source/JWTPlugin/Private/JWTVerifier.cpp
+++ Source/JWTPlugin/Private/JWTVerifier.cpp
@@ -2,22 +2,25 @@
 #include "JWTVerifier.h"
 
 void UJWTVerifier::SetAlgorithm(const FString& Key, EAlgorithm Algorithm) {
-	switch (Algorithm) {
-		case EAlgorithm::hs256: Verifier = Verifier.allow_algorithm(jwt::algorithm::hs256{ TCHAR_TO_ANSI(*Key) }); break;
-		case EAlgorithm::hs384: Verifier = Verifier.allow_algorithm(jwt::algorithm::hs384{ TCHAR_TO_ANSI(*Key) }); break;
-		case EAlgorithm::hs512: Verifier = Verifier.allow_algorithm(jwt::algorithm::hs256{ TCHAR_TO_ANSI(*Key) }); break;
-		case EAlgorithm::rs256: Verifier = Verifier.allow_algorithm(jwt::algorithm::rs256(TCHAR_TO_ANSI(*Key), "", "", "")); break;
-		case EAlgorithm::rs384: Verifier = Verifier.allow_algorithm(jwt::algorithm::rs384(TCHAR_TO_ANSI(*Key), "", "", "")); break;
-		case EAlgorithm::rs512: Verifier = Verifier.allow_algorithm(jwt::algorithm::rs512(TCHAR_TO_ANSI(*Key), "", "", "")); break;
-		/*case Algorithm::ed25519:Verifier = Verifier.allow_algorithm(jwt::algorithm::ed25519{ TCHAR_TO_ANSI(*key) }); break;
-		case Algorithm::ed448:Verifier = Verifier.allow_algorithm(jwt::algorithm::ed448{ TCHAR_TO_ANSI(*key) }); break;
-		case Algorithm::es256:Verifier = Verifier.allow_algorithm(jwt::algorithm::es256{ TCHAR_TO_ANSI(*key) }); break;
-		case Algorithm::es384:Verifier = Verifier.allow_algorithm(jwt::algorithm::es384{ TCHAR_TO_ANSI(*key) }); break;
-		case Algorithm::es512:Verifier = Verifier.allow_algorithm(jwt::algorithm::es512{ TCHAR_TO_ANSI(*key) }); break;
-		case Algorithm::ps256:Verifier = Verifier.allow_algorithm(jwt::algorithm::hs512{ TCHAR_TO_ANSI(*key) }); break;
-		case Algorithm::ps384:Verifier = Verifier.allow_algorithm(jwt::algorithm::ps384{ TCHAR_TO_ANSI(*key) }); break;
-		case Algorithm::ps512:Verifier = Verifier.allow_algorithm(jwt::algorithm::ps512{ TCHAR_TO_ANSI(*key) }); break;*/
-	}	
+	try
+	{
+		switch (Algorithm) {
+			case EAlgorithm::hs256: Verifier = Verifier.allow_algorithm(jwt::algorithm::hs256{ TCHAR_TO_ANSI(*Key) }); break;
+			case EAlgorithm::hs384: Verifier = Verifier.allow_algorithm(jwt::algorithm::hs384{ TCHAR_TO_ANSI(*Key) }); break;
+			case EAlgorithm::hs512: Verifier = Verifier.allow_algorithm(jwt::algorithm::hs256{ TCHAR_TO_ANSI(*Key) }); break;
+			case EAlgorithm::rs256: Verifier = Verifier.allow_algorithm(jwt::algorithm::rs256(TCHAR_TO_ANSI(*Key), "", "", "")); break;
+			case EAlgorithm::rs384: Verifier = Verifier.allow_algorithm(jwt::algorithm::rs384(TCHAR_TO_ANSI(*Key), "", "", "")); break;
+			case EAlgorithm::rs512: Verifier = Verifier.allow_algorithm(jwt::algorithm::rs512(TCHAR_TO_ANSI(*Key), "", "", "")); break;
+			/*case Algorithm::ed25519:Verifier = Verifier.allow_algorithm(jwt::algorithm::ed25519{ TCHAR_TO_ANSI(*key) }); break;
+			case Algorithm::ed448:Verifier = Verifier.allow_algorithm(jwt::algorithm::ed448{ TCHAR_TO_ANSI(*key) }); break;
+			case Algorithm::es256:Verifier = Verifier.allow_algorithm(jwt::algorithm::es256{ TCHAR_TO_ANSI(*key) }); break;
+			case Algorithm::es384:Verifier = Verifier.allow_algorithm(jwt::algorithm::es384{ TCHAR_TO_ANSI(*key) }); break;
+			case Algorithm::es512:Verifier = Verifier.allow_algorithm(jwt::algorithm::es512{ TCHAR_TO_ANSI(*key) }); break;
+			case Algorithm::ps256:Verifier = Verifier.allow_algorithm(jwt::algorithm::hs512{ TCHAR_TO_ANSI(*key) }); break;
+			case Algorithm::ps384:Verifier = Verifier.allow_algorithm(jwt::algorithm::ps384{ TCHAR_TO_ANSI(*key) }); break;
+			case Algorithm::ps512:Verifier = Verifier.allow_algorithm(jwt::algorithm::ps512{ TCHAR_TO_ANSI(*key) }); break;*/
+		}	
+	} catch(...) {}
 }
 
 bool UJWTVerifier::VerifyJWT(const FString& Input) {
