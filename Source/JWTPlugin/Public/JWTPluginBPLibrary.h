86260b187a0fbff88a5b78b8e55459da90081958
whcooper <95888123+whcooper@users.noreply.github.com>
2022-08-26 21:09:11 -0400

    Add JWTPLUGIN_API to UJWTPluginBPLibrary class

--- Source/JWTPlugin/Public/JWTPluginBPLibrary.h
+++ Source/JWTPlugin/Public/JWTPluginBPLibrary.h
@@ -10,7 +10,7 @@
 
 
 UCLASS()
-class UJWTPluginBPLibrary : public UBlueprintFunctionLibrary
+class JWTPLUGIN_API UJWTPluginBPLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_UCLASS_BODY()
 
